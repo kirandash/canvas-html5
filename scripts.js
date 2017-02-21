f4304983e6631b60ab35b5a6ada0635829a857c6
kirandash <thekirankumardash@gmail.com>
2017-02-21 22:41:19 +0530

    3 Adding More Circles

--- scripts.js
+++ scripts.js
@@ -7,11 +7,25 @@ function myCircleAnimation() {
 
 	function drawCircle(){
 		context.clearRect(0,0,300,300);//Clear canvas everytime animation runs [x,y,width,height]
+		
 		context.beginPath();
 		context.arc(initialX,initialX,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
 		context.fillStyle = "#0000ff"; //Color to be used
 		context.fill();//Fill the color
 		context.stroke();
+		
+		context.beginPath();
+		context.arc(initialX,150,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
+		context.fillStyle = "#00ff80"; //Color to be used
+		context.fill();//Fill the color
+		context.stroke();
+
+		context.beginPath();
+		context.arc(initialX,60,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
+		context.fillStyle = "#bf0ff"; //Color to be used
+		context.fill();//Fill the color
+		context.stroke();
+
 		if(initialX<finalX){
 			initialX++;
 		}
