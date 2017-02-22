ee774f16727384f5389f1bfe25faebb9fbaf1388
kirandash <thekirankumardash@gmail.com>
2017-02-22 21:41:08 +0530

    4 Draw a Line

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function myCircleAnimation() {
+function canvasAnimation() {
 	var myCanvas = document.getElementById("myCanvas");
 	var context = myCanvas.getContext("2d");
 	var initialX = 50,
@@ -9,20 +9,20 @@ function myCircleAnimation() {
 		context.clearRect(0,0,300,300);//Clear canvas everytime animation runs [x,y,width,height]
 		
 		context.beginPath();
-		context.arc(initialX,initialX,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
+		context.arc(50,initialX-30,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
 		context.fillStyle = "#0000ff"; //Color to be used
 		context.fill();//Fill the color
 		context.stroke();
 		
 		context.beginPath();
-		context.arc(initialX,150,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
+		context.arc(initialX,100,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
 		context.fillStyle = "#00ff80"; //Color to be used
 		context.fill();//Fill the color
 		context.stroke();
 
 		context.beginPath();
 		context.arc(initialX,60,40, 0, 2*Math.PI);//initial x y, radius, initial and ending angle
-		context.fillStyle = "#bf0ff"; //Color to be used
+		context.fillStyle = "#bf0ff0"; //Color to be used
 		context.fill();//Fill the color
 		context.stroke();
 
@@ -32,5 +32,18 @@ function myCircleAnimation() {
 		requestAnimationFrame(drawCircle); //Build the animation with request - for smooth animation
 	}
 
+	function drawLine(){
+		var myCanvas = document.getElementById("lineCanvas");
+		var context = myCanvas.getContext("2d");
+		context.fillStyle = "#ff1a1a";
+		context.fillRect(0,0,300,300); //To fill color in a rectangle
+		context.moveTo(0,0);
+		context.lineTo(300,300);
+		context.moveTo(300,0);
+		context.lineTo(0,300);
+		context.stroke();
+	}
+
 	drawCircle();
+	drawLine();
 }
